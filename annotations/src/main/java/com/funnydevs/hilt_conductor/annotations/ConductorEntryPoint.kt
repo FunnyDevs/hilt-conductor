e00ab8480ac81ec31846b3587d1db0f8ed94fc73
Pierfrancesco Gulinelli <pierfrancesco.gulinelli@intellitronika.com>
2021-05-07 09:17:51 +0200

    clean dependencies

--- annotations/src/main/java/com/funnydevs/hilt_conductor/annotations/ConductorEntryPoint.kt
+++ annotations/src/main/java/com/funnydevs/hilt_conductor/annotations/ConductorEntryPoint.kt
@@ -4,9 +4,5 @@ import kotlin.reflect.KClass
 
 @Target(AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.CLASS)
 annotation class ConductorEntryPoint(
-  /**
-   * The base class for the generated Hilt class. When applying the Hilt Gradle Plugin this value
-   * is not necessary and will be inferred from the current superclass.
-   */
   val value: KClass<*> = Void::class
 )
